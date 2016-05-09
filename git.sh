b4a3657cc93b02241e64c5e251de2c727ced2189
asmlib <asminsights@gmail.com>
2016-05-09 13:11:24 +0800

    first commit

---
+++ git.sh
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+git init
+git add *
+git commit -m "first commit"
+git remote add origin https://github.com/asmlib/gperftools.git
+git push -u origin master
+
---
+++ install.sh
@@ -0,0 +1,19 @@
+
+
+sh autogen.sh && \
+./configure make && make install && \
+sudo /sbin/ldconfig && \
+mkdir /tmp/tcmalloc && \
+chmod 0777 /tmp/tcmalloc 
+
+
+
+
+
+
+
+
+
+
+
+
